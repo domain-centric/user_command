bad562c622fe648e0c323f63236b0d03aeb04872
Nils ten Hoeve <nilstenhoeve@gmail.com>
2021-07-01 12:26:09 +0200

    Fixed #9

--- lib/src/command_style.dart
+++ lib/src/command_style.dart
@@ -13,20 +13,19 @@ import 'package:flutter/material.dart';
 /// Deviations must be explained in the doc comments (behind ///)
 
 class CommandStyle {
-
   ///From [Material design][https://material.io/design/layout/spacing-methods.html#touch-targets]:
   ///Touch targets apply to any device that receives both touch and non-touch input.
   ///To balance information density and usability,
   ///touch targets should be at least 48 x 48 dp with at least 8dp of space between targets.
   ///
   /// TODO: can this be less for less dense application (e.g. with mouse)?
-  static final minTouchTargetHeight = 48.0;
+  static const touchTargetHeight = 48.0;
 
   ///From [Material design][https://material.io/design/layout/spacing-methods.html#touch-targets]:
   ///touch targets should be at least 48 x 48 dp with at least 8dp of space between targets.
   ///
   /// TODO: can this be less for less dense application (e.g. with mouse)?
-  static final spacing = 20.0;
+  static const spacing = 20.0;
 
   ///Rounded shapes are important:
   ///- Rounded corners look visually appealing. TODO ADD REFERENCE
@@ -46,26 +45,106 @@ class CommandStyle {
   static final rounding = 16.0;
   static final Radius radius = Radius.circular(rounding);
   static final RoundedRectangleBorder roundedRectangleBorder =
-  RoundedRectangleBorder(borderRadius: BorderRadius.all(radius));
+      RoundedRectangleBorder(borderRadius: BorderRadius.all(radius));
+}
 
-// TODO move to ButtonStyle
-// static ThemeData _genericTheme(
-//         {required MaterialColor primarySwatch,
-//         required Brightness brightness}) =>
-//     ThemeData(
-//         primarySwatch: primarySwatch,
-//         brightness: brightness,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//         textButtonTheme: TextButtonThemeData(
-//             style: TextButton.styleFrom(
-//               ///Adding padding because these buttons are too small by default
-//               ///From [Material design][https://material.io/design/layout/spacing-methods.html#touch-targets]:
-//               ///Touch targets apply to any device that receives both touch and non-touch input.
-//               ///To balance information density and usability,
-//               ///touch targets should be at least 48 x 48 dp with at least 8dp of space between targets.
-//                 padding: EdgeInsets.all(spacing),
-//                 /// We use a bigger radius. See [rounding]
-//                 shape: roundedRectangleBorder)));
+class CommandButtonStyle extends ButtonStyle {
+  /// Constrains the button size since fixedSize, minimumSize and tapTargetSize parameters
+  /// did not work as expected
+  final BoxConstraints? constraints;
 
+  /// not using fixedSize: use constraints instead
+  /// not using minimumSize: use constraints instead
+  /// not using tapTargetSize: use constraints instead,
+  const CommandButtonStyle(
+      {this.constraints,
+      MaterialStateProperty<Color?>? foregroundColor,
+      AlignmentGeometry? alignment,
+      Duration? animationDuration,
+      MaterialStateProperty<Color?>? backgroundColor,
+      MaterialStateProperty<double?>? elevation,
+      bool? enableFeedback,
+      MaterialStateProperty<MouseCursor?>? mouseCursor,
+      MaterialStateProperty<Color?>? overlayColor,
+      MaterialStateProperty<EdgeInsetsGeometry?>? padding,
+      MaterialStateProperty<Color?>? shadowColor,
+      MaterialStateProperty<OutlinedBorder?>? shape,
+      MaterialStateProperty<BorderSide?>? side,
+      InteractiveInkFeatureFactory? splashFactory,
+      MaterialStateProperty<TextStyle?>? textStyle,
+      VisualDensity? visualDensity})
+      : super(
+            foregroundColor: foregroundColor,
+            alignment: alignment,
+            animationDuration: animationDuration,
+            backgroundColor: backgroundColor,
+            elevation: elevation,
+            enableFeedback: enableFeedback,
+            //use constraints instead
+            fixedSize: null,
+            //use constraints instead
+            minimumSize: null,
+            mouseCursor: mouseCursor,
+            overlayColor: overlayColor,
+            padding: padding,
+            shadowColor: shadowColor,
+            shape: shape,
+            side: side,
+            splashFactory: splashFactory,
+            //use constraints instead
+            tapTargetSize: null,
+            textStyle: textStyle,
+            visualDensity: visualDensity);
 
+  /// Creates a CommandButtonStyle with given values, but overridden with
+  /// default values where needed if the original values are null.
+  /// These default values should come from the current theme.
+  ///
+  /// not using fixedSize: use constraints instead
+  /// not using minimumSize: use constraints instead
+  /// not using tapTargetSize: use constraints instead,
+  CommandButtonStyle overrideDefaultValues(
+      {MaterialStateProperty<Color?>? defaultForegroundColor,
+      AlignmentGeometry? defaultAlignment,
+      Duration? defaultAnimationDuration,
+      MaterialStateProperty<Color?>? defaultBackgroundColor,
+      MaterialStateProperty<double?>? defaultElevation,
+      bool? defaultEnableFeedback,
+      MaterialStateProperty<MouseCursor?>? defaultMouseCursor,
+      MaterialStateProperty<Color?>? defaultOverlayColor,
+      MaterialStateProperty<Color?>? defaultShadowColor,
+      MaterialStateProperty<BorderSide?>? defaultSide,
+      InteractiveInkFeatureFactory? defaultSplashFactory,
+      MaterialStateProperty<TextStyle?>? defaultTextStyle,
+      VisualDensity? defaultVisualDensity}) {
+    return CommandButtonStyle(
+        constraints: constraints ?? defaultConstraints(),
+        foregroundColor: foregroundColor ?? defaultForegroundColor,
+        alignment: alignment ?? defaultAlignment,
+        animationDuration: animationDuration ?? defaultAnimationDuration,
+        backgroundColor: backgroundColor ?? defaultBackgroundColor,
+        elevation: elevation ?? defaultElevation,
+        enableFeedback: enableFeedback ?? defaultEnableFeedback,
+        mouseCursor: mouseCursor ?? defaultMouseCursor,
+        overlayColor: overlayColor ?? defaultOverlayColor,
+        padding: padding ?? defaultPadding(),
+        shadowColor: shadowColor ?? defaultShadowColor,
+        shape: shape ?? defaultShape(),
+        side: side ?? defaultSide,
+        splashFactory: splashFactory ?? defaultSplashFactory,
+        textStyle: textStyle ?? defaultTextStyle,
+        visualDensity: visualDensity ?? defaultVisualDensity);
+  }
+
+  BoxConstraints defaultConstraints() => BoxConstraints(
+        minHeight: CommandStyle.touchTargetHeight,
+      );
+
+  MaterialStateProperty<EdgeInsetsGeometry?> defaultPadding() =>
+      MaterialStateProperty.all<EdgeInsetsGeometry?>(EdgeInsets.fromLTRB(
+          CommandStyle.spacing, 0, CommandStyle.spacing, 0));
+
+  MaterialStateProperty<OutlinedBorder?> defaultShape() =>
+      MaterialStateProperty.all<OutlinedBorder?>(
+          CommandStyle.roundedRectangleBorder);
 }
--- lib/src/command_toolbar.dart
+++ lib/src/command_toolbar.dart
@@ -20,14 +20,14 @@ class CommandToolbar extends StatelessWidget {
     Color backGroundColor = Theme.of(context).dialogBackgroundColor;
 
     List<Command> visibleCommands =
-    commands.where((command) => command.visible).toList();
+        commands.where((command) => command.visible).toList();
     return Visibility(
         visible: visibleCommands.isNotEmpty,
         child: Container(
             padding: EdgeInsets.fromLTRB(
                 CommandStyle.spacing, 0, CommandStyle.spacing, 0),
             color: backGroundColor,
-            height: CommandStyle.minTouchTargetHeight,
+            height: CommandStyle.touchTargetHeight,
             child: Align(
               alignment: Alignment.centerRight,
               child: OverflowView.flexible(
@@ -55,7 +55,7 @@ class CommandToolBarMoreButton extends StatelessWidget {
     Color foreGroundColor = Theme.of(context).textTheme.bodyText1!.color!;
 
     return ConstrainedBox(
-      constraints: BoxConstraints(minHeight: CommandStyle.minTouchTargetHeight),
+      constraints: BoxConstraints(minHeight: CommandStyle.touchTargetHeight),
       child: TextButton(
         key: buttonKey,
         style: TextButton.styleFrom(
@@ -101,19 +101,18 @@ class CommandToolbarButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    CommandToolbarButtonStyle styleWithDefaults =
-    style.withDefaultValues(context);
+    CommandButtonStyle styleWithDefaults = style.withDefaultValues(context);
 
     IconData? iconData = command.icon;
     if (iconData == null) {
       return ConstrainedBox(
-        constraints: styleWithDefaults.constraints!
-        //wont be null after style.withDefaultValues
-        ,
+        constraints:
+            // wont be null after style.withDefaultValues
+            styleWithDefaults.constraints!,
         child: TextButton(
-          child: Text(command.name)
-          // we are not using CommandText because we are using styleWithDefaults.textStyle instead
-          ,
+          child:
+              // not using CommandText because we are using styleWithDefaults.textStyle instead
+              Text(command.name),
           style: styleWithDefaults,
           onPressed: () {
             command.action();
@@ -122,18 +121,19 @@ class CommandToolbarButton extends StatelessWidget {
       );
     } else {
       return ConstrainedBox(
-        constraints: styleWithDefaults.constraints!
-        //wont be null after style.withDefaultValues
-        ,
+        constraints:
+            // wont be null after style.withDefaultValues
+            styleWithDefaults.constraints!,
         child: TextButton.icon(
           style: styleWithDefaults,
-          label: Text(
+          label:
+              // not using CommandText because we are using styleWithDefaults.textStyle instead
+              Text(
             command.name,
-          )
-          // we are not using CommandText because we are using styleWithDefaults.textStyle instead
-          ,
-          icon: Icon(command.icon),
-          // we are not using CommandIcon because we are using styleWithDefaults.textStyle instead
+          ),
+          icon:
+              // using CommandIcon because we are using styleWithDefaults.textStyle instead
+              Icon(command.icon),
           onPressed: () {
             command.action();
           },
@@ -143,107 +143,76 @@ class CommandToolbarButton extends StatelessWidget {
   }
 }
 
-
-class CommandToolbarButtonStyle extends ButtonStyle {
-  final BoxConstraints?
-  constraints; //todo do we need this? see minimumSize and tapTargetSize
-
-  const CommandToolbarButtonStyle({this.constraints,
-    MaterialStateProperty<Color?>? foregroundColor,
-    AlignmentGeometry? alignment,
-    Duration? animationDuration,
-    MaterialStateProperty<Color?>? backgroundColor,
-    MaterialStateProperty<double?>? elevation,
-    bool? enableFeedback,
-    MaterialStateProperty<Size?>? fixedSize,
-    MaterialStateProperty<Size?>? minimumSize,
-    MaterialStateProperty<MouseCursor?>? mouseCursor,
-    MaterialStateProperty<Color?>? overlayColor,
-    MaterialStateProperty<EdgeInsetsGeometry?>? padding,
-    MaterialStateProperty<Color?>? shadowColor,
-    MaterialStateProperty<OutlinedBorder?>? shape,
-    MaterialStateProperty<BorderSide?>? side,
-    InteractiveInkFeatureFactory? splashFactory,
-    MaterialTapTargetSize? tapTargetSize,
-    MaterialStateProperty<TextStyle?>? textStyle,
-    VisualDensity? visualDensity})
+@immutable
+class CommandToolbarButtonStyle extends CommandButtonStyle {
+  const CommandToolbarButtonStyle(
+      {BoxConstraints? constraints,
+      MaterialStateProperty<Color?>? foregroundColor,
+      AlignmentGeometry? alignment,
+      Duration? animationDuration,
+      MaterialStateProperty<Color?>? backgroundColor,
+      MaterialStateProperty<double?>? elevation,
+      bool? enableFeedback,
+      MaterialStateProperty<MouseCursor?>? mouseCursor,
+      MaterialStateProperty<Color?>? overlayColor,
+      MaterialStateProperty<EdgeInsetsGeometry?>? padding,
+      MaterialStateProperty<Color?>? shadowColor,
+      MaterialStateProperty<OutlinedBorder?>? shape,
+      MaterialStateProperty<BorderSide?>? side,
+      InteractiveInkFeatureFactory? splashFactory,
+      MaterialStateProperty<TextStyle?>? textStyle,
+      VisualDensity? visualDensity})
       : super(
-      foregroundColor: foregroundColor,
-      alignment: alignment,
-      animationDuration: animationDuration,
-      backgroundColor: backgroundColor,
-      elevation: elevation,
-      enableFeedback: enableFeedback,
-      fixedSize: fixedSize,
-      minimumSize: minimumSize,
-      mouseCursor: mouseCursor,
-      overlayColor: overlayColor,
-      padding: padding,
-      shadowColor: shadowColor,
-      shape: shape,
-      side: side,
-      splashFactory: splashFactory,
-      tapTargetSize: tapTargetSize,
-      textStyle: textStyle,
-      visualDensity: visualDensity);
+            constraints: constraints,
+            foregroundColor: foregroundColor,
+            animationDuration: animationDuration,
+            visualDensity: visualDensity,
+            textStyle: textStyle,
+            splashFactory: splashFactory,
+            side: side,
+            shape: shape,
+            shadowColor: shadowColor,
+            padding: padding,
+            overlayColor: overlayColor,
+            mouseCursor: mouseCursor,
+            enableFeedback: enableFeedback,
+            elevation: elevation,
+            backgroundColor: backgroundColor,
+            alignment: alignment);
 
-  /// Use given values, but if they are null use default values where needed,
-  /// based on the current theme
-  CommandToolbarButtonStyle withDefaultValues(BuildContext context) {
-    Color defaultForegroundColor =
-    Theme.of(context).textTheme.bodyText1!.color!;
-    return CommandToolbarButtonStyle(
-        constraints: constraints ?? defaultConstraints(),
-        foregroundColor: foregroundColor ??
-            MaterialStateProperty.all<Color>(defaultForegroundColor),
-        alignment: alignment,
-        animationDuration: animationDuration,
-        backgroundColor: backgroundColor,
-        elevation: elevation,
-        enableFeedback: enableFeedback,
-        fixedSize: fixedSize,
-        minimumSize: minimumSize,
-        mouseCursor: mouseCursor,
-        overlayColor:
-        overlayColor ?? _TextButtonDefaultOverlay(defaultForegroundColor),
-        padding: padding ?? defaultPadding(),
-        shadowColor: shadowColor,
-        shape: shape ?? defaultShape(),
-        side: side,
-        splashFactory: splashFactory,
-        tapTargetSize: tapTargetSize,
-        textStyle: textStyle,
-        visualDensity: visualDensity);
+  CommandButtonStyle withDefaultValues(BuildContext context) {
+    Color foregroundColor = Theme.of(context).textTheme.bodyText1!.color!;
+    return overrideDefaultValues(
+        defaultForegroundColor: _DefaultForegroundColor(foregroundColor),
+        defaultOverlayColor: _DefaultOverlayColor(foregroundColor));
   }
-
-  BoxConstraints defaultConstraints() => BoxConstraints(
-    minHeight: CommandStyle.minTouchTargetHeight,
-  );
-
-  MaterialStateProperty<EdgeInsetsGeometry?> defaultPadding() =>
-      MaterialStateProperty.all<EdgeInsetsGeometry?>(EdgeInsets.fromLTRB(
-          CommandStyle.spacing, 0, CommandStyle.spacing, 0));
-
-  MaterialStateProperty<OutlinedBorder?> defaultShape() =>
-      MaterialStateProperty.all<OutlinedBorder?>(
-          CommandStyle.roundedRectangleBorder);
 }
-
 
 /// Inspired by [TextButton.styleFrom()]
 @immutable
-class _TextButtonDefaultOverlay extends MaterialStateProperty<Color?> {
-  _TextButtonDefaultOverlay(this.primary);
+class _DefaultForegroundColor extends MaterialStateProperty<Color?> {
+  _DefaultForegroundColor(this.color);
 
-  final Color primary;
+  final Color color;
 
   @override
   Color? resolve(Set<MaterialState> states) {
-    if (states.contains(MaterialState.hovered))
-      return primary.withOpacity(0.04);
+    return color;
+  }
+}
+
+/// Inspired by [TextButton.styleFrom()]
+@immutable
+class _DefaultOverlayColor extends MaterialStateProperty<Color?> {
+  _DefaultOverlayColor(this.color);
+
+  final Color color;
+
+  @override
+  Color? resolve(Set<MaterialState> states) {
+    if (states.contains(MaterialState.hovered)) return color.withOpacity(0.04);
     if (states.contains(MaterialState.focused) ||
-        states.contains(MaterialState.pressed))
-      return primary.withOpacity(0.12);
+        states.contains(MaterialState.pressed)) return color.withOpacity(0.12);
     return null;
   }
 }
